c9f6403aae3dc1de561a8371481861ae84da5b13
Michael K-F <63935978+MichaelK-F@users.noreply.github.com>
2022-03-16 19:58:32 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+Chrome dino
+
+index - standard version.html :has the standard version of chrome dino.
+
+index - bot version.html :has the bot version (runs by itself, but you can intervine by pressing up/down keys and space bar) of chrome dino.
+
+index - hacked version.html :the dino goes through everything and nothing effeccts it.
+
+index - hacked version + bot version.html :the dino goes through everything and nothing effeccts it, and runs by itself, but you can intervine by pressing up/down keys and space bar
+
---
+++ gpio.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+host_name = '10.92.5.11'    # Change this to your Raspberry Pi IP address
+host_port = 8000
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """ A special implementation of BaseHTTPRequestHander for reading data from
+        and control GPIO of a Raspberry Pi
+    """
+
+    def do_HEAD(self):
+        """ do_HEAD() can be tested use curl command 
+            'curl -I http://server-ip-address:port' 
+        """
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        """ do_GET() can be tested using curl command 
+            'curl http://server-ip-address:port' 
+        """
+        html = '''
+            <html>
+            <body style="width:960px; margin: 20px auto;">
+            <h1>Welcome to my Raspberry Pi</h1>
+            <p>Current GPU temperature is {}</p>
+            <p>Turn LED: <a href="/on">On</a> <a href="/off">Off</a></p>
+            <div id="led-status"></div>
+            <script>
+                document.getElementById("led-status").innerHTML="{}";
+            </script>
+            </body>
+            </html>
+        '''
+        temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
+        self.do_HEAD()
+        status = ''
+        if self.path=='/':
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
+            GPIO.setup(18, GPIO.OUT)
+        elif self.path=='/on':
+            GPIO.output(18, GPIO.HIGH)
+            status='LED is On'
+        elif self.path=='/off':
+            GPIO.output(18, GPIO.LOW)
+            status='LED is Off'
+        self.wfile.write(html.format(temp[5:], status).encode("utf-8"))
