321db07a4bdb8a77916365660331f01216b76016
Michael K-F <michael@wandoo.ca>
2022-08-27 19:58:22 +0800

    *sigh*

---
+++ pointerpointer.com/precache-manifest.3a9e260eab37fc695ab41585606a1b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44894110495f3e64ea4547b5c329a66d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "054459ca3f7d60b6402f",
+    "url": "/static/css/main.326af8f0.chunk.css"
+  },
+  {
+    "revision": "ef2e5614893374207d94",
+    "url": "/static/js/2.7bc288b0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7bc288b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "054459ca3f7d60b6402f",
+    "url": "/static/js/main.919b6db1.chunk.js"
+  },
+  {
+    "revision": "b422de5154558c839768",
+    "url": "/static/js/runtime-main.4ee85aee.js"
+  }
+]);
